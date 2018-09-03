6c4a0444839e43c4f589ce1b799d06726a29e8a0
Benjamim Thiago <bebenjamimthiago@gmail.com>
2018-09-03 14:39:37 -0300

    ajuste do tempo do token

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,8 +28,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
-        Passport::tokensExpireIn(Carbon::now()->addHours(10));
-        Passport::refreshTokensExpireIn(Carbon::now()->addDays(1));
+        Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
+        Passport::refreshTokensExpireIn(Carbon::now()->addMinutes(60));
+		//Passport::refreshTokensExpireIn(Carbon::now()->addDays(1));
 
         //
     }
